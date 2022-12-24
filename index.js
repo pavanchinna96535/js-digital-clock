4e1bee03b4d6beac849e3c391c122edd5a31b328
pavanchinna96535 <116867109+pavanchinna96535@users.noreply.github.com>
2022-12-24 15:11:50 +0530

    clock.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+function displaytime(){
+
+    var date=new Date();
+    
+    if(getHours()>12){
+        displaytime=(date.getHours()-12)+":"+date.getMinutes()+":"+date.getSeconds();
+    }
+
+    if(getHours()>12){
+        document.getElementById("ip2").innerHTML="PM";
+    }
+    else{
+        document.getElementById("ip2").innerHTML="AM";
+    }
+    
+    
+    document.getElementById("ip1").innerHTML=displaytime;
+    
+    }
+    setInterval(displaytime,10);
